83089bac9aad4d7957b4ebc008522402674c70c9
shaiksha1290 <shaikshavali.1290@gmail.com>
2015-12-31 18:48:16 -0500

    Delete file

--- Individual_project.R
+++
@@ -1,8 +0,0 @@
-library(ggplot2)
-
-loan_data <- read.csv("Subset of Loans Modified fall 2015.csv")
-
-names(loan_data)
-loan_data$int_rate <- as.numeric(strsplit(as.character(loan_data$int_rate),split = "%"))
-
-qplot(x=loan_data$int_rate, data = loan_data) 
